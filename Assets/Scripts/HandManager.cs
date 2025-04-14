0c3d5537bd3fc6f5a261251737b8a9d4938cb769
kzoo8 <kellyzhuy906@gmail.com>
2025-04-13 23:09:12 -0700

    fhgjhklhblkjgh

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -4,8 +4,11 @@ public class HandManager : MonoBehavior
 {
    [field: SerializedField] public GameObject cardPrefab {get;set;} // Assign card prefab in inspector
 
+   [field: SerializedField] public Transform handTransform {get;set} // Root of the hand positions
+
+   [f]
     void Start() 
     {
-        
+
     }
 }
