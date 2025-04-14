c9b414016287058983831589293a2b90190045dc
Ryan-Fue <ryanfue@ucla.edu>
2025-04-13 17:49:49 -0700

    Created DisplayCard.cs and Level1 Scene
    Level1 scene allows for screen display of a card
    
    DisplayCard.cs updates card with correct information at start

---
+++ Assets/Scripts/DisplayCard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Scales;
+
+public class DisplayCard : MonoBehaviour
+{
+    [field: SerializeField] public Card cardData {get;set;}
+    [field: SerializeField] public Image cardImage {get;set;}
+    [field: SerializeField] public TMP_Text nameText {get;set;}
+    [field: SerializeField] public TMP_Text healthText {get;set;}
+    [field: SerializeField] public TMP_Text damageText {get;set;}
+    [field: SerializeField] public Image[] typeImages {get;set;}
+
+
+    void Start()
+    {
+        UpdateCardDisplay();
+    }
+
+    public void UpdateCardDisplay()
+    {
+        nameText.text = cardData.CardName;
+        healthText.text = cardData.Health.ToString();
+
+        // Will display range of damage (Functional string?) e.g. 6-8
+        damageText.text = $"{cardData.DamageMin} - {cardData.DamageMax}";
+        
+    }
+
+}
