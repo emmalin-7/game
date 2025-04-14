6bdaec469996406f636318736cb86ff90a9c6f28
kzoo8 <kellyzhuy906@gmail.com>
2025-04-13 23:41:15 -0700

    new

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -1,14 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using Scales;
-public class HandManager : MonoBehavior
+
+public class HandManager : MonoBehaviour
 {
-   [field: SerializedField] public GameObject cardPrefab {get;set;} // Assign card prefab in inspector
+    [field: SerializeField] public GameObject cardPrefab  // Assign card prefab in inspector
 
-   [field: SerializedField] public Transform handTransform {get;set} // Root of the hand positions
+    [field: SerializeField] public Transform handTransform // Root of the hand positions
 
-   [f]
+   [field: SerializeField] public float fanSpread = 5f 
+
+   [field: SerializeField] public List<GameObject> cardsInHand = new List<GameObject>()// Hold a list of card objects in our hand
+
+
     void Start() 
     {
-
+      AddCardToHand();
     }
+
+   public void AddCardToHand() 
+   {
+      // Instantiate the card
+      GameObject newCard = Instantiate(cardPrefab, handTransform.positions), Quaternion.identity)
+   }
+
+
 }
