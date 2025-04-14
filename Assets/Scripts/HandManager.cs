42a50d347e14149e0dd39cae1c796b904f13be19
emmalin <emailtoemma@gmail.com>
2025-04-14 00:44:33 -0700

    ehehehehe

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Scales;
+using System;
 
 public class HandManager : MonoBehaviour
 {
@@ -19,13 +20,29 @@ public class HandManager : MonoBehaviour
     void Start() 
     {
       AddCardToHand();
+      AddCardToHand();
+      AddCardToHand();
+      AddCardToHand();
+      AddCardToHand();
+      AddCardToHand();
     }
-    public void AddCardToHand() 
-   {
-      // Instantiate the card
-   }
 
+    public void AddCardToHand()
+    {
+        GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
+        cardsInHand.Add(newCard);
 
-   
+        UpdateHandVisuals();
+    }
 
+    private void UpdateHandVisuals()
+    {
+        int cardCount = cardsInHand.Count;
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            float rotationAngle = (fanSpread * (i - (cardCount - 1) / 2f));
+            cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+        }
+    }
 }
